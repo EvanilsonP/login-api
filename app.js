495256ca3198d13a958930ef4e689dc9efff4d80
EvanilsonP <pevanilson99@gmail.com>
2023-03-09 13:12:52 -0300

    handling errors

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const routes = require('./routes/authRoutes');
 const PORT = 3000;
+const cookieParser = require('cookie-parser');
 const db = require('./database/db');
 db.database();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => { res.send('Welcome to this page.')});
 app.use(routes);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,35 @@
 const User = require('../models/User');
 
+// handling errors
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { email: '', password: '' };
+
+    // incorrect email
+    if(err.message === 'incorrect email') {
+        errors.email = 'that email is already registered'
+    }
+
+  // incorrect password
+  if (err.message === 'incorrect password') {
+    errors.password = 'That password is incorrect';
+  }
+
+  // duplicate email error
+  if (err.code === 11000) {
+    errors.email = 'that email is already registered';
+    return errors;
+  }
+
+  // validation errors
+  if (err.message.includes('user validation failed')) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+};
+
 const signup = (req, res) => {
     res.render('signup');
 };
@@ -21,7 +51,15 @@ const signup_post = async (req, res) => {
 };
 
 const login_post = (req, res) => {
-    res.send('Login post.');
+    const { email, password } = req.body;
+
+    try {
+        
+    } 
+    
+    catch (err) {
+        
+    }
 };
 
 module.exports = { signup, login, signup_post, login_post };
